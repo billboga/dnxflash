524008f30464b6d99111c07b7cab228e7e9400ee
William Boga IV <williambogaiv@gmail.com>
2015-07-16 20:39:02 -0400

    Add initial solution files. This includes a starter message class along with a few basic tests.

---
+++ src/MarqueeMessenger/MarqueeMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MarqueeMessenger
+{
+    public class MarqueeMessage
+    {
+        public MarqueeMessage(
+            string message,
+            string title = null,
+            string type = null)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            Message = message;
+            Title = title;
+            Type = type;
+        }
+
+        public string Message { get; private set; }
+        public string Title { get; private set; }
+        public string Type { get; private set; }
+    }
+}
---
+++ test/MarqueeMessenger.Tests/MarqueeMessageTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xunit;
+
+namespace MarqueeMessenger.Tests
+{
+    public class MarqueeMessageTests
+    {
+        private MarqueeMessage sut;
+
+        public class Constructor : MarqueeMessageTests
+        {
+            [Fact]
+            public void Should_set_message()
+            {
+                sut = new MarqueeMessage("test message");
+
+                Assert.Equal("test message", sut.Message);
+            }
+
+            [Theory,
+                InlineData(null),
+                InlineData("")]
+            public void Should_throw_exception_if_message_is_not_valid(string message)
+            {
+                var exception = Assert.Throws<ArgumentNullException>(() => new MarqueeMessage(message));
+
+                Assert.Equal("message", exception.ParamName);
+            }
+
+            [Fact]
+            public void Should_set_title()
+            {
+                sut = new MarqueeMessage(
+                    message: "test message",
+                    title: "test");
+
+                Assert.Equal("test", sut.Title);
+            }
+
+            [Fact]
+            public void Should_set_type()
+            {
+                sut = new MarqueeMessage(
+                    message: "test message",
+                    type: "test");
+
+                Assert.Equal("test", sut.Type);
+            }
+        }
+    }
+}
