c80dad38d80435386f397bf511ebdd66344751a1
William Boga IV <williambogaiv@gmail.com>
2015-07-19 13:01:15 -0400

    Add AspNet session message provider. This currently works with dnx beta5 leveraging ISessionCollection. This is to matchup with AspNet MVC's usage of the interface. It appears beta6+ will use ISession.

---
+++ src/MarqueeMessenger.AspNet.SessionMessageProvider/SessionMessageProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Http;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace MarqueeMessenger.AspNet.SessionMessageProvider
+{
+    public class SessionMessageProvider : IMessageProvider
+    {
+        public SessionMessageProvider(ISessionCollection session)
+        {
+            this.session = session;
+        }
+
+        private readonly ISessionCollection session;
+        private const string sessionKey = "marquee-messenger-session-message-provider";
+
+        public object Get()
+        {
+            byte[] value;
+            object messages = null;
+
+            if (session.TryGetValue(sessionKey, out value))
+            {
+                if (value != null)
+                {
+                    messages =
+                        JsonConvert.DeserializeObject(Encoding.UTF8.GetString(value));
+                }
+            }
+
+            return messages;
+        }
+
+        public void Set(object messages)
+        {
+            session.Set(
+                sessionKey,
+                new ArraySegment<byte>(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messages))));
+        }
+    }
+}
