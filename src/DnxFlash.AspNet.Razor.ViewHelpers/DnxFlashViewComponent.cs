237eeaa515127ac091c21306e86487873db4e93a
William Boga IV <williambogaiv@gmail.com>
2015-08-09 20:24:50 -0400

    Add Razor view-helper
    Helper is a trial at using new `ViewComponent` feature of MVC6.

---
+++ src/DnxFlash.AspNet.Razor.ViewHelpers/DnxFlashViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+
+namespace DnxFlash.AspNet.Razor.ViewHelpers
+{
+    public class DnxFlashViewComponent : ViewComponent
+    {
+        public DnxFlashViewComponent(IMessenger messenger)
+        {
+            this.messenger = messenger;
+        }
+
+        private readonly IMessenger messenger;
+
+        public IViewComponentResult Invoke()
+        {
+            return Invoke(null);
+        }
+
+        public IViewComponentResult Invoke(string view)
+        {
+            var messages = new List<Message>();
+            Message message = null;
+
+            while ((message = messenger.Fetch()) != null)
+            {
+                messages.Add(message);
+            }
+
+            return View(view, messages);
+        }
+    }
+}
